6bae88c96010a87cfb65022eeb30bd23f004d408
Adriano Raiano <adriano@raiano.ch>
2021-12-07 13:22:21 +0100

    other comment

--- example/lib/redirect.js
+++ example/lib/redirect.js
@@ -9,7 +9,7 @@ export const useRedirect = (to) => {
   // language detection
   useEffect(() => {
     const detectedLng = languageDetector.detect()
-    if (to.startsWith('/' + detectedLng) && router.route === '/404') {
+    if (to.startsWith('/' + detectedLng) && router.route === '/404') { // prevent endless loop
       router.replace('/' + detectedLng + router.route)
       return
     }
