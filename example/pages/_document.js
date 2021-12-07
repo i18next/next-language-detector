73460a156fcccbb7cf6bfbb59f0dd701f0509ddb
Adriano Raiano <adriano@raiano.ch>
2021-12-07 13:44:14 +0100

    cosmetics

--- example/pages/_document.js
+++ example/pages/_document.js
@@ -1,9 +1,9 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
-import i18nextConfig from '../next-i18next.config';
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+import i18nextConfig from '../next-i18next.config'
 
 class MyDocument extends Document {
   render() {
-    const currentLocale = this.props.__NEXT_DATA__.query.locale || i18nextConfig.i18n.defaultLocale;
+    const currentLocale = this.props.__NEXT_DATA__.query.locale || i18nextConfig.i18n.defaultLocale
     return (
       <Html lang={currentLocale}>
         <Head />
@@ -12,8 +12,8 @@ class MyDocument extends Document {
           <NextScript />
         </body>
       </Html>
-    );
+    )
   }
 }
 
-export default MyDocument;
+export default MyDocument
