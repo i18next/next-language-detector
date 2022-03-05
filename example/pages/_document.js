93760460ff5356adf7c6e7d05bd671e601ffa635
Adriano Raiano <adriano@raiano.ch>
2022-03-05 22:03:03 +0100

    update deps in example

--- example/pages/_document.js
+++ example/pages/_document.js
@@ -6,7 +6,14 @@ class MyDocument extends Document {
     const currentLocale = this.props.__NEXT_DATA__.query.locale || i18nextConfig.i18n.defaultLocale
     return (
       <Html lang={currentLocale}>
-        <Head />
+        <Head>
+          <link href='https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css' rel='stylesheet' />
+          <link href='/app.css' rel='stylesheet' />
+
+          <link href='https://cdnjs.cloudflare.com/ajax/libs/typicons/2.0.9/typicons.min.css' rel='stylesheet' />
+          <link href='https://fonts.googleapis.com/css?family=Open+Sans:300,400|Oswald:600' rel='stylesheet' />
+          <link data-react-helmet='true' rel='icon' href='https://blobscdn.gitbook.com/v0/b/gitbook-28427.appspot.com/o/spaces%2F-L9iS6Wm2hynS5H9Gj7j%2Favatar.png?generation=1523462254548780&amp;alt=media' />
+        </Head>
         <body>
           <Main />
           <NextScript />
