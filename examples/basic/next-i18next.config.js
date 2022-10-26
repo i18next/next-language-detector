a2d0a013e61c23901bd0cdafa504fcd93b454550
Adriano Raiano <adrai@users.noreply.github.com>
2022-10-26 22:19:13 +0200

    example: reloadOnPrerender fixes #5

--- examples/basic/next-i18next.config.js
+++ examples/basic/next-i18next.config.js
@@ -1,7 +1,15 @@
+// used for SSR (getServerSideProps)
+// const path = require('path')
+// const localePath = path.resolve('./public/locales')
+
 module.exports = {
-  // debug: true,
+  // https://www.i18next.com/overview/configuration-options#logging
+  debug: process.env.NODE_ENV === 'development',
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'de', 'it'],
   },
+  // localePath,
+  reloadOnPrerender: process.env.NODE_ENV === 'development',
+  // serializeConfig: false,
 }
